342807aa101cca2ca82c377d5ac85e5198dc2708
Sodaman <mattrick2214@gmail.com>
2022-01-23 13:11:09 -0600

    altered db.json to be read

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const { test } = require('./db/db.json');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.get('/api/text', (req, res) => {
-    res.send('Hello!')
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/api/test', (req, res) => {
+    res.json(test);
 });
 
 app.listen(PORT, () => {
