598ac775174a905564efb50484eb1955c3568a59
Sodaman <mattrick2214@gmail.com>
2022-01-23 13:25:50 -0600

    working on getting get started to bring us to notes

---
+++ routes/htmlRoutes/ApiRoutes/index.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const noteRoutes = require('../ApiRoutes/noteRoutes');
+
+router.use(noteRoutes);
+
+modules.exports = router;
---
+++ routes/htmlRoutes/ApiRoutes/noteRoutes.js
@@ -0,0 +1,7 @@
+// const router = require('express').Router();
+// const { notes } = requires('../../db/db.json');
+
+// router.get('/notes', (req, res) =>{
+//     let results = notes;
+//     if
+// })
